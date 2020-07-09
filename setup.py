b6d6cdb4681977adc34ab2e4b7fd66d46a2bd2de
aldencolerain <aldencolerain@gmail.com>
2020-07-09 14:57:21 -0600

    Create example pip package with pytest, nox, and distutils

---
+++ setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import sys
+from setuptools import setup, find_packages
+
+
+# keep this package private
+if set(sys.argv).intersection(['upload', 'register']):
+    print('This setup is private and should not be uploaded or registered.')
+    sys.exit(-1)
+
+# package settings
+setup(
+
+    # info
+    name='expip',
+    version='0.0.1',
+    author='Alden',
+    url='https://github.com/aldencolerain/expip.git',
+    description='Example pip package.',
+    long_description=open('README.md').read(),
+
+    # include
+    packages=find_packages(exclude=["tests"]),
+    include_package_data=True,
+
+    # public dependencies
+    install_requires=[
+        'pyyaml==5.3.1'],
+
+    # keep this package private
+    classifiers=['Private :: Do Not Upload'],
+
+    # cli scripts
+    entry_points={'console_scripts': ['expip = expip.cli:main']},
+)
